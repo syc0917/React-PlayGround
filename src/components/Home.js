fee37b354c4810c99306bfa0cd6e31562def3029
syc0917 <48739240+syc0917@users.noreply.github.com>
2021-02-28 20:28:00 +1100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,29 @@
 import logo from '../logo.svg';
+import { makeStyles } from '@material-ui/core/styles';
 import { Link } from "react-router-dom";
 import Button from '@material-ui/core/Button';
 
+const useStyles = makeStyles((theme) => ({
+    link: {
+        color: 'white',
+        textDecoration: 'none'
+    }
+}));
+
 function Home(props){
+    const classes = useStyles();
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                 
-                建立新的页面<code>src/components</code> 接着在<code>App.js</code>添加路由指向新页面, 并在<code>src/components/Home.js</code>添加<Link />Button
+                在<code>src/components</code>建立新的页面, 接着在<code>App.js</code>添加路由指向新页面, 并在<code>src/components/Home.js</code>添加<Link />Button.
                 </p>
                 <Button>
-                <Link to="/Theme">
-                    Theme
-                </Link>
+                    <Link className={classes.link} to="/Theme">
+                        Theme
+                    </Link>
                 </Button>
                 
             </header>
